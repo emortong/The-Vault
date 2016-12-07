f22be1ace47e5eff77e3d4e2145ecf6114d706f8
Estefania Morton <estefania.morton@gmail.com>
2016-12-06 18:45:33 -1000

    finished revealing module pattern

--- vault.js
+++ vault.js
@@ -1,4 +1,37 @@
 'use strict';
 module.exports = function() {
-  
+    var data = {};
+
+    // return {
+
+    //   setValue: function(key, value) {
+    //     data[key] = value;
+    //   },
+
+    //   getValue: function(key) {
+    //     if(data[key]) {
+    //       return data[key];
+    //     } else {
+    //       return null;
+    //     }
+    //   }
+    // }
+
+    function setValue(key, value) {
+        data[key] = value;
+      }
+
+    function getValue(key) {
+        if(data[key]) {
+          return data[key];
+        } else {
+          return null;
+        }
+      }
+
+      return {
+        setValue: setValue,
+        getValue: getValue
+      }
+
 };
